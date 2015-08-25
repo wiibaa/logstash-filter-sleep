fffb80e067076c376c130226ae6ae4b65c00bd16
wiibaa <philippeweber57@yahoo.fr>
2015-08-25 06:43:02 +0200

    test for replay and time sprintf value

--- spec/filters/sleep_spec.rb
+++ spec/filters/sleep_spec.rb
@@ -47,5 +47,82 @@ describe LogStash::Filters::Sleep do
       end
 
     end
+
+   context "when using replay mode" do
+
+      let(:messages) { 20 }
+      subject { LogStash::Filters::Sleep.new("time" => time, "replay" => true ) }
+
+      before(:each) do
+        subject.register
+      end
+
+      it "should sleep for N seconds and continue" do
+        expect(subject).to receive(:sleep).with(0.2).exactly(19).times
+        messages.times do
+          event.timestamp = event.timestamp + 1
+          subject.filter(event)
+        end
+      end
+
+    end
+  end
+
+  describe "sleep for a event-based time" do
+
+    let(:time) { "%{time}" }
+
+    before(:each) do
+      subject.register
+      event["time"] = 4
+    end
+
+    it "should sleep for N seconds and continue" do
+      expect(subject).to receive(:sleep).with(4)
+      subject.filter(event)
+    end
+
+    context "when using every N events" do
+
+      let(:messages) { 20 }
+      let(:every) { 5 }
+      subject { LogStash::Filters::Sleep.new("time" => time, "every" => every ) }
+
+      before(:each) do
+        subject.register
+      end
+
+      it "should sleep for N seconds and continue" do
+        expect(subject).to receive(:sleep).with(5).exactly(1).times
+        expect(subject).to receive(:sleep).with(10).exactly(1).times
+        expect(subject).to receive(:sleep).with(15).exactly(1).times
+        expect(subject).to receive(:sleep).with(20).exactly(1).times
+        messages.times do |i|
+          event["time"] = i+1
+          subject.filter(event)
+        end
+      end
+
+    end
+
+    context "when using replay mode" do
+
+      let(:messages) { 20 }
+      subject { LogStash::Filters::Sleep.new("time" => time, "replay" => true ) }
+
+      before(:each) do
+        subject.register
+        event["time"] = 4
+      end
+
+      it "should sleep for N seconds and continue" do
+        expect(subject).to receive(:sleep).with(0.25).exactly(19).times
+        messages.times do
+          event.timestamp = event.timestamp + 1
+          subject.filter(event)
+        end
+      end
+
+    end
   end
 end
